9f52133883b4a2ab4709a8137e8faabb509b6367
Thomas SAMTER <samter.Thomas@gmail.com>
2022-11-08 13:58:43 +0100

    Add CI workflow (#2)
    * feat(ci) : init flow CI
    
    * feat(ci) : lock esphome version
    
    * fix(ci) : adding raw component
    
    * fix(ci) : update components to match CI
    
    * feat(tools) : adding pre-commit

---
+++ components/rhasspy_satellite/rhasspy_satellite.cpp
@@ -0,0 +1,16 @@
+#include "esphome/core/log.h"
+#include "rhasspy_satellite.h"
+
+namespace esphome {
+namespace rhasspy_satellite {
+
+static const char *const TAG = "rhasspy_satellite.component";
+
+void RhasspySatelliteComponent::setup() {}
+
+void RhasspySatelliteComponent::loop() {}
+
+void RhasspySatelliteComponent::dump_config() { ESP_LOGCONFIG(TAG, "RhasspySatellite Component"); }
+
+}  // namespace rhasspy_satellite
+}  // namespace esphome
---
+++ components/rhasspy_satellite/rhasspy_satellite.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace rhasspy_satellite {
+
+class RhasspySatelliteComponent : public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+};
+
+}  // namespace rhasspy_satellite
+}  // namespace esphome
