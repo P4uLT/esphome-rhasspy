44aa834ff9df4046954697af7201a5ef901b1613
P4uLT <samter.thomas@gmail.com>
2022-11-09 21:08:47 +0000

    refactor(inmp441) : switching to config

--- components/inmp441_stream/inmp441_stream.cpp
+++ components/inmp441_stream/inmp441_stream.cpp
@@ -1,34 +1,29 @@
-#include "esphome/core/log.h"
 #include "inmp441_stream.h"
 
-#include "AudioTools.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace inmp441_stream {
 
-static const char *TAG = "inmp441_stream.component";
+static const char *const TAG = "inmp441_stream.component";
 
-uint16_t sample_rate = 22500;
-uint16_t channels = 2;
-uint16_t bits_per_sample = 16;  // or try with 24 or 32
-
-I2SStream in;
-I2SStream out;
+audio_tools::I2SStream in;
+audio_tools::I2SStream out;
 
 // Effect
 // Boost boost(1);
 // AudioEffects<GeneratorFromStream<effect_t>> effects(in,channels,1.0);
 // GeneratedSoundStream<int16_t> in_boosted(effects);
 
-StreamCopy copier(out, in);  // copies sound into i2s
+Inmp441StreamComponent::Inmp441StreamComponent() { this->copier_ = make_unique<StreamCopy>(); }
 
 void Inmp441StreamComponent::setup() {
   ESP_LOGCONFIG(TAG, "Setting up Audio...");
 
   // start I2S in
-  auto config_in = in.defaultConfig(RX_MODE);
-  config_in.sample_rate = sample_rate;
-  config_in.bits_per_sample = bits_per_sample;
+  audio_tools::I2SConfig config_in = in.defaultConfig(RX_MODE);
+  config_in.sample_rate = this->sample_rate_;
+  config_in.bits_per_sample = this->bits_per_sample_;
   config_in.i2s_format = I2S_STD_FORMAT;
   config_in.is_master = true;
   config_in.use_apll = true,
@@ -44,9 +39,10 @@ void Inmp441StreamComponent::setup() {
   // effects.addEffect(boost);
 
   // start I2S out
-  auto config_out = out.defaultConfig(TX_MODE);
-  config_out.sample_rate = sample_rate;
-  config_out.bits_per_sample = bits_per_sample;
+  audio_tools::I2SConfig config_out = out.defaultConfig(TX_MODE);
+  config_out.sample_rate = this->sample_rate_;
+  config_out.bits_per_sample = this->bits_per_sample_;
+  ;
   config_out.i2s_format = I2S_STD_FORMAT;
   config_out.is_master = true;
   config_out.port_no = 1;
@@ -56,18 +52,27 @@ void Inmp441StreamComponent::setup() {
   config_out.use_apll = true,
 
   out.begin(config_out);
+  this->copier_->begin(out, in);
 }
 
 void Inmp441StreamComponent::loop() {
-  if (this->output_switch_ != nullptr && this->output_switch_->state == true) {
+  if (this->output_switch_ != nullptr && this->output_switch_->state) {
     this->mute_binary_sensor_->publish_state(false);
-    copier.copy();
+    this->copier_->copy();
   } else {
     this->mute_binary_sensor_->publish_state(true);
   }
 }
 
-void Inmp441StreamComponent::dump_config() { ESP_LOGCONFIG(TAG, "Inmp441StreamComponent Component"); }
+void Inmp441StreamComponent::dump_config() {
+  ESP_LOGCONFIG(TAG, "Inmp441StreamComponent:");
+  ESP_LOGCONFIG(TAG, "  Sample Rate: %u", this->sample_rate_);
+  ESP_LOGCONFIG(TAG, "  Channels: %u", this->channels_);
+  ESP_LOGCONFIG(TAG, "  Bits Per Sample: %u", this->bits_per_sample_);
+  ESP_LOGCONFIG(TAG, "  SCK Pin: %u", this->sck_pin_);
+  ESP_LOGCONFIG(TAG, "  WS  Pin: %u", this->ws_pin_);
+  ESP_LOGCONFIG(TAG, "  SD  Pin: %u", this->sd_pin_);
+}
 
 }  // namespace inmp441_stream
 }  // namespace esphome
--- components/inmp441_stream/inmp441_stream.h
+++ components/inmp441_stream/inmp441_stream.h
@@ -4,6 +4,8 @@
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/switch/switch.h"
 
+#include "AudioTools.h"
+
 // #include "StateMachine.hpp"
 
 namespace esphome {
@@ -11,6 +13,7 @@ namespace inmp441_stream {
 
 class Inmp441StreamComponent : public Component {
  public:
+  Inmp441StreamComponent();
   void setup() override;
   float get_setup_priority() const override { return esphome::setup_priority::LATE; }
 
@@ -22,6 +25,10 @@ class Inmp441StreamComponent : public Component {
   void set_ws_pin(uint8_t pin) { this->ws_pin_ = pin; }
   void set_sd_pin(uint8_t pin) { this->sd_pin_ = pin; }
 
+  void set_sample_rate(uint16_t sample_rate) { this->sample_rate_ = sample_rate; }
+  void set_channels(uint8_t channels) { this->channels_ = channels; }
+  void set_bits_per_sample(uint8_t bits_per_sample) { this->bits_per_sample_ = bits_per_sample; }
+
   void set_mute_binary_sensor(binary_sensor::BinarySensor *mute_binary_sensor) {
     mute_binary_sensor_ = mute_binary_sensor;
   }
@@ -29,12 +36,17 @@ class Inmp441StreamComponent : public Component {
   void set_output_switch(switch_::Switch *output_switch) { output_switch_ = output_switch; }
 
  protected:
-  binary_sensor::BinarySensor *mute_binary_sensor_;
-  switch_::Switch *output_switch_;
+  std::unique_ptr<StreamCopy> copier_;
+  binary_sensor::BinarySensor *mute_binary_sensor_{nullptr};
+  switch_::Switch *output_switch_{nullptr};
 
-  uint8_t sck_pin_;
-  uint8_t ws_pin_;
-  uint8_t sd_pin_;
+  uint16_t sample_rate_{};
+  uint8_t channels_{};
+  uint8_t bits_per_sample_{};
+
+  uint8_t sck_pin_{};
+  uint8_t ws_pin_{};
+  uint8_t sd_pin_{};
 };
 
 }  // namespace inmp441_stream
