11e843c2575de47e6eb66545684f1a6f8b5347d5
P4uLT <samter.thomas@gmail.com>
2022-11-16 15:28:58 +0000

    feat(ci) : append new deps

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,7 +5,3 @@ set -e
 if [ ! -L esphome ]; then
     ln -sv $(python3 -c "import esphome as _; print(_.__path__[0])") esphome
 fi
-
-pio lib -e esp32-arduino  install https://github.com/pschatzmann/arduino-audio-tools
-pio lib -e esp32-arduino  install https://github.com/digint/tinyfsm
-pio init --ide vscode -e esp32-arduino
--- script/append.py
+++ script/append.py
@@ -1,16 +1,51 @@
 #!/usr/bin/env python3
 from configparser import ConfigParser
+import argparse
+import os
+import sys
 
-file = "esphome/platformio.ini"
-section = "common:esp32-arduino"
+root_path = os.path.abspath(os.path.normpath(os.path.join(__file__, "..", "..")))
 key = "lib_deps"
-config = ConfigParser()
-config.read(file)
 
-esp32_arduino = config.options(section)
-lib_deps = config.get(section, key).split("\n")
-lib_deps.append("https://github.com/pschatzmann/arduino-audio-tools")
-config.set(section, "lib_deps", "\n".join(lib_deps))
 
-with open(file, "w") as configfile:  # save
-    config.write(configfile)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--path",
+        help="the PlatformIO environment path",
+    )
+
+    args = parser.parse_args()
+
+    additionnal_libs_deps_file = os.path.join(
+        root_path, "script/additionnal_libs_deps.ini"
+    )
+
+    file = f"{root_path}/{args.path}/platformio.ini"
+
+    additionnal_lib_config = ConfigParser()
+    additionnal_lib_config.read(additionnal_libs_deps_file)
+    for section in additionnal_lib_config.sections():
+        additionnal_lib_deps = additionnal_lib_config.get(section, key).split("\n")
+
+        platformio_config = ConfigParser()
+        platformio_config.read(file)
+
+        lib_deps = platformio_config.get(section, key).split("\n")
+        changed = False
+
+        for lib in additionnal_lib_deps:
+            if lib not in lib_deps:
+                lib_deps.append(lib)
+                print(f"Adding {lib} to section {section}")
+                changed = True
+
+        if changed:
+            platformio_config.set(section, key, "\n".join(lib_deps))
+            with open(file, "w", encoding="utf-8") as configfile:  # save
+                platformio_config.write(configfile)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
