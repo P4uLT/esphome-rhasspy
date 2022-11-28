823bfd79378c00643364650a9e75f1d18638daf7
P4uLT <7680607+P4uLT@users.noreply.github.com>
2022-11-28 16:05:45 +0000

    fix(es8388) : update based on CI

--- components/es8388/es8388_component.cpp
+++ components/es8388/es8388_component.cpp
@@ -6,89 +6,89 @@ namespace esphome {
 namespace es8388 {
 
 /* ES8388 register */
-#define ES8388_CONTROL1 0x00
-#define ES8388_CONTROL2 0x01
-#define ES8388_CHIPPOWER 0x02
-#define ES8388_ADCPOWER 0x03
-#define ES8388_DACPOWER 0x04
-#define ES8388_CHIPLOPOW1 0x05
-#define ES8388_CHIPLOPOW2 0x06
-#define ES8388_ANAVOLMANAG 0x07
-#define ES8388_MASTERMODE 0x08
+const uint8_t ES8388_CONTROL1 = 0x00;
+const uint8_t ES8388_CONTROL2 = 0x01;
+const uint8_t ES8388_CHIPPOWER = 0x02;
+const uint8_t ES8388_ADCPOWER = 0x03;
+const uint8_t ES8388_DACPOWER = 0x04;
+const uint8_t ES8388_CHIPLOPOW1 = 0x05;
+const uint8_t ES8388_CHIPLOPOW2 = 0x06;
+const uint8_t ES8388_ANAVOLMANAG = 0x0;
+const uint8_t ES8388_MASTERMODE = 0x08;
 
 /* ADC */
-#define ES8388_ADCCONTROL1 0x09
-#define ES8388_ADCCONTROL2 0x0a
-#define ES8388_ADCCONTROL3 0x0b
-#define ES8388_ADCCONTROL4 0x0c
-#define ES8388_ADCCONTROL5 0x0d
-#define ES8388_ADCCONTROL6 0x0e
-#define ES8388_ADCCONTROL7 0x0f
-#define ES8388_ADCCONTROL8 0x10
-#define ES8388_ADCCONTROL9 0x11
-#define ES8388_ADCCONTROL10 0x12
-#define ES8388_ADCCONTROL11 0x13
-#define ES8388_ADCCONTROL12 0x14
-#define ES8388_ADCCONTROL13 0x15
-#define ES8388_ADCCONTROL14 0x16
+const uint8_t ES8388_ADCCONTROL1 = 0x09;
+const uint8_t ES8388_ADCCONTROL2 = 0x0a;
+const uint8_t ES8388_ADCCONTROL3 = 0x0b;
+const uint8_t ES8388_ADCCONTROL4 = 0x0c;
+const uint8_t ES8388_ADCCONTROL5 = 0x0d;
+const uint8_t ES8388_ADCCONTROL6 = 0x0e;
+const uint8_t ES8388_ADCCONTROL7 = 0x0f;
+const uint8_t ES8388_ADCCONTROL8 = 0x10;
+const uint8_t ES8388_ADCCONTROL9 = 0x11;
+const uint8_t ES8388_ADCCONTROL10 = 0x12;
+const uint8_t ES8388_ADCCONTROL11 = 0x13;
+const uint8_t ES8388_ADCCONTROL12 = 0x14;
+const uint8_t ES8388_ADCCONTROL13 = 0x15;
+const uint8_t ES8388_ADCCONTROL14 = 0x16;
 
 /* DAC */
-#define ES8388_DACCONTROL1 0x17
-#define ES8388_DACCONTROL2 0x18
-#define ES8388_DACCONTROL3 0x19
-#define ES8388_DACCONTROL4 0x1a
-#define ES8388_DACCONTROL5 0x1b
-#define ES8388_DACCONTROL6 0x1c
-#define ES8388_DACCONTROL7 0x1d
-#define ES8388_DACCONTROL8 0x1e
-#define ES8388_DACCONTROL9 0x1f
-#define ES8388_DACCONTROL10 0x20
-#define ES8388_DACCONTROL11 0x21
-#define ES8388_DACCONTROL12 0x22
-#define ES8388_DACCONTROL13 0x23
-#define ES8388_DACCONTROL14 0x24
-#define ES8388_DACCONTROL15 0x25
-#define ES8388_DACCONTROL16 0x26
-#define ES8388_DACCONTROL17 0x27
-#define ES8388_DACCONTROL18 0x28
-#define ES8388_DACCONTROL19 0x29
-#define ES8388_DACCONTROL20 0x2a
-#define ES8388_DACCONTROL21 0x2b
-#define ES8388_DACCONTROL22 0x2c
-#define ES8388_DACCONTROL23 0x2d
-#define ES8388_DACCONTROL24 0x2e
-#define ES8388_DACCONTROL25 0x2f
-#define ES8388_DACCONTROL26 0x30
-#define ES8388_DACCONTROL27 0x31
-#define ES8388_DACCONTROL28 0x32
-#define ES8388_DACCONTROL29 0x33
-#define ES8388_DACCONTROL30 0x34
+const uint8_t ES8388_DACCONTROL1 = 0x17;
+const uint8_t ES8388_DACCONTROL2 = 0x18;
+const uint8_t ES8388_DACCONTROL3 = 0x19;
+const uint8_t ES8388_DACCONTROL4 = 0x1a;
+const uint8_t ES8388_DACCONTROL5 = 0x1b;
+const uint8_t ES8388_DACCONTROL6 = 0x1c;
+const uint8_t ES8388_DACCONTROL7 = 0x1d;
+const uint8_t ES8388_DACCONTROL8 = 0x1e;
+const uint8_t ES8388_DACCONTROL9 = 0x1f;
+const uint8_t ES8388_DACCONTROL10 = 0x20;
+const uint8_t ES8388_DACCONTROL11 = 0x21;
+const uint8_t ES8388_DACCONTROL12 = 0x22;
+const uint8_t ES8388_DACCONTROL13 = 0x23;
+const uint8_t ES8388_DACCONTROL14 = 0x24;
+const uint8_t ES8388_DACCONTROL15 = 0x25;
+const uint8_t ES8388_DACCONTROL16 = 0x26;
+const uint8_t ES8388_DACCONTROL17 = 0x27;
+const uint8_t ES8388_DACCONTROL18 = 0x28;
+const uint8_t ES8388_DACCONTROL19 = 0x29;
+const uint8_t ES8388_DACCONTROL20 = 0x2a;
+const uint8_t ES8388_DACCONTROL21 = 0x2b;
+const uint8_t ES8388_DACCONTROL22 = 0x2c;
+const uint8_t ES8388_DACCONTROL23 = 0x2d;
+const uint8_t ES8388_DACCONTROL24 = 0x2e;
+const uint8_t ES8388_DACCONTROL25 = 0x2f;
+const uint8_t ES8388_DACCONTROL26 = 0x30;
+const uint8_t ES8388_DACCONTROL27 = 0x31;
+const uint8_t ES8388_DACCONTROL28 = 0x32;
+const uint8_t ES8388_DACCONTROL29 = 0x33;
+const uint8_t ES8388_DACCONTROL30 = 0x34;
 
 /**
  * @brief (un)mute one of the two outputs or main dac output of the ES8388 by switching of the output register bits.
- * Does not really mute the selected output, causes an attenuation. hence should be used in conjunction with appropriate
- * volume setting. Main dac output mute does mute both outputs
+ * Does not really mute the selected output, causes an attenuation. hence should be used in conjunction with
+ * appropriate volume setting. Main dac output mute does mute both outputs
  *
  * @param out
  * @param muted
  */
-void ES8388Component::mute(const ES8388_OUT out, const bool muted) {
+void ES8388Component::mute(const OutputType out, const bool muted) {
   uint8_t reg_addr;
   uint8_t mask_mute;
   uint8_t mask_val;
 
   switch (out) {
-    case ES_OUT1:
+    case OUTPUT_TYPE_OUT1:
       reg_addr = ES8388_DACPOWER;
       mask_mute = (3 << 4);
       mask_val = muted ? 0 : mask_mute;
       break;
-    case ES_OUT2:
+    case OUTPUT_TYPE_OUT2:
       reg_addr = ES8388_DACPOWER;
       mask_mute = (3 << 2);
       mask_val = muted ? 0 : mask_mute;
       break;
-    case ES_MAIN:
+    case OUTPUT_TYPE_MAIN:
     default:
       reg_addr = ES8388_DACCONTROL3;
       mask_mute = 1 << 2;
@@ -110,23 +110,24 @@ void ES8388Component::mute(const ES8388_OUT out, const bool muted) {
  * @param out which gain setting to control
  * @param vol 0-100 (100 is max)
  */
-void ES8388Component::volume(const ES8388_OUT out, const uint8_t vol) {
+void ES8388Component::volume(const OutputType out, const uint8_t vol) {
   const uint32_t max_vol = 100;  // max input volume value
 
-  const int32_t max_vol_val = out == ES8388_OUT::ES_MAIN ? 96 : 0x21;  // max register value for ES8388 out volume
+  const int32_t max_vol_val =
+      out == OutputType::OUTPUT_TYPE_MAIN ? 96 : 0x21;  // max register value for ES8388 out volume
 
   uint8_t lreg = 0, rreg = 0;
 
   switch (out) {
-    case ES_MAIN:
+    case OUTPUT_TYPE_MAIN:
       lreg = ES8388_DACCONTROL4;
       rreg = ES8388_DACCONTROL5;
       break;
-    case ES_OUT1:
+    case OUTPUT_TYPE_OUT1:
       lreg = ES8388_DACCONTROL24;
       rreg = ES8388_DACCONTROL25;
       break;
-    case ES_OUT2:
+    case OUTPUT_TYPE_OUT2:
       lreg = ES8388_DACCONTROL26;
       rreg = ES8388_DACCONTROL27;
       break;
@@ -136,7 +137,7 @@ void ES8388Component::volume(const ES8388_OUT out, const uint8_t vol) {
 
   // main dac volume control is reverse scale (lowest value is loudest)
   // hence we reverse the calculated value
-  if (out == ES_MAIN) {
+  if (out == OUTPUT_TYPE_MAIN) {
     vol_val = max_vol_val - vol_val;
   }
 
@@ -190,8 +191,8 @@ void ES8388Component::setup() {
   this->write_bytes(ES8388_ADCCONTROL9, {0x20});
 
   /* set LOUT1 / ROUT1 volume: 0dB (unattenuated) */
-  this->write_bytes(ES8388_DACCONTROL24, {33});  // 0x1e
-  this->write_bytes(ES8388_DACCONTROL25, {33});
+  this->write_bytes(ES8388_DACCONTROL24, {0x1e});  // 0x1e
+  this->write_bytes(ES8388_DACCONTROL25, {0x1e});
 
   /* set LOUT2 / ROUT2 volume: 0dB (unattenuated) */
   this->write_bytes(ES8388_DACCONTROL26, {0x1e});
--- components/es8388/es8388_component.h
+++ components/es8388/es8388_component.h
@@ -6,17 +6,17 @@
 namespace esphome {
 namespace es8388 {
 
-enum ES8388_OUT {
-  ES_MAIN,  // this is the DAC output volume (both outputs)
-  ES_OUT1,  // this is the additional gain for OUT1
-  ES_OUT2   // this is the additional gain for OUT2
+enum OutputType {
+  OUTPUT_TYPE_MAIN,  // this is the DAC output volume (both outputs)
+  OUTPUT_TYPE_OUT1,  // this is the additional gain for OUT1
+  OUTPUT_TYPE_OUT2   // this is the additional gain for OUT2
 };
 
 class ES8388Component : public Component, public i2c::I2CDevice {
  public:
   void setup() override;
-  void mute(const ES8388_OUT out, const bool muted);
-  void volume(const ES8388_OUT out, const uint8_t vol);
+  void mute(const OutputType out, const bool muted);
+  void volume(const OutputType out, const uint8_t vol);
   float get_setup_priority() const override { return setup_priority::LATE - 1; }
 };
 
