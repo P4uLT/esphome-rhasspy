9ba58f9cbb32dc9166c975f8c55fed6c4007baa0
P4uLT <7680607+P4uLT@users.noreply.github.com>
2022-11-28 16:10:56 +0000

    fix(es8388) : update based on CI

--- components/es8388/es8388_component.cpp
+++ components/es8388/es8388_component.cpp
@@ -6,63 +6,63 @@ namespace esphome {
 namespace es8388 {
 
 /* ES8388 register */
-const uint8_t ES8388_CONTROL1 = 0x00;
-const uint8_t ES8388_CONTROL2 = 0x01;
-const uint8_t ES8388_CHIPPOWER = 0x02;
-const uint8_t ES8388_ADCPOWER = 0x03;
-const uint8_t ES8388_DACPOWER = 0x04;
-const uint8_t ES8388_CHIPLOPOW1 = 0x05;
-const uint8_t ES8388_CHIPLOPOW2 = 0x06;
-const uint8_t ES8388_ANAVOLMANAG = 0x0;
-const uint8_t ES8388_MASTERMODE = 0x08;
+static const uint8_t ES8388_CONTROL1 = 0x00;
+static const uint8_t ES8388_CONTROL2 = 0x01;
+static const uint8_t ES8388_CHIPPOWER = 0x02;
+static const uint8_t ES8388_ADCPOWER = 0x03;
+static const uint8_t ES8388_DACPOWER = 0x04;
+static const uint8_t ES8388_CHIPLOPOW1 = 0x05;
+static const uint8_t ES8388_CHIPLOPOW2 = 0x06;
+static const uint8_t ES8388_ANAVOLMANAG = 0x0;
+static const uint8_t ES8388_MASTERMODE = 0x08;
 
 /* ADC */
-const uint8_t ES8388_ADCCONTROL1 = 0x09;
-const uint8_t ES8388_ADCCONTROL2 = 0x0a;
-const uint8_t ES8388_ADCCONTROL3 = 0x0b;
-const uint8_t ES8388_ADCCONTROL4 = 0x0c;
-const uint8_t ES8388_ADCCONTROL5 = 0x0d;
-const uint8_t ES8388_ADCCONTROL6 = 0x0e;
-const uint8_t ES8388_ADCCONTROL7 = 0x0f;
-const uint8_t ES8388_ADCCONTROL8 = 0x10;
-const uint8_t ES8388_ADCCONTROL9 = 0x11;
-const uint8_t ES8388_ADCCONTROL10 = 0x12;
-const uint8_t ES8388_ADCCONTROL11 = 0x13;
-const uint8_t ES8388_ADCCONTROL12 = 0x14;
-const uint8_t ES8388_ADCCONTROL13 = 0x15;
-const uint8_t ES8388_ADCCONTROL14 = 0x16;
+static const uint8_t ES8388_ADCCONTROL1 = 0x09;
+static const uint8_t ES8388_ADCCONTROL2 = 0x0a;
+static const uint8_t ES8388_ADCCONTROL3 = 0x0b;
+static const uint8_t ES8388_ADCCONTROL4 = 0x0c;
+static const uint8_t ES8388_ADCCONTROL5 = 0x0d;
+static const uint8_t ES8388_ADCCONTROL6 = 0x0e;
+static const uint8_t ES8388_ADCCONTROL7 = 0x0f;
+static const uint8_t ES8388_ADCCONTROL8 = 0x10;
+static const uint8_t ES8388_ADCCONTROL9 = 0x11;
+static const uint8_t ES8388_ADCCONTROL10 = 0x12;
+static const uint8_t ES8388_ADCCONTROL11 = 0x13;
+static const uint8_t ES8388_ADCCONTROL12 = 0x14;
+static const uint8_t ES8388_ADCCONTROL13 = 0x15;
+static const uint8_t ES8388_ADCCONTROL14 = 0x16;
 
 /* DAC */
-const uint8_t ES8388_DACCONTROL1 = 0x17;
-const uint8_t ES8388_DACCONTROL2 = 0x18;
-const uint8_t ES8388_DACCONTROL3 = 0x19;
-const uint8_t ES8388_DACCONTROL4 = 0x1a;
-const uint8_t ES8388_DACCONTROL5 = 0x1b;
-const uint8_t ES8388_DACCONTROL6 = 0x1c;
-const uint8_t ES8388_DACCONTROL7 = 0x1d;
-const uint8_t ES8388_DACCONTROL8 = 0x1e;
-const uint8_t ES8388_DACCONTROL9 = 0x1f;
-const uint8_t ES8388_DACCONTROL10 = 0x20;
-const uint8_t ES8388_DACCONTROL11 = 0x21;
-const uint8_t ES8388_DACCONTROL12 = 0x22;
-const uint8_t ES8388_DACCONTROL13 = 0x23;
-const uint8_t ES8388_DACCONTROL14 = 0x24;
-const uint8_t ES8388_DACCONTROL15 = 0x25;
-const uint8_t ES8388_DACCONTROL16 = 0x26;
-const uint8_t ES8388_DACCONTROL17 = 0x27;
-const uint8_t ES8388_DACCONTROL18 = 0x28;
-const uint8_t ES8388_DACCONTROL19 = 0x29;
-const uint8_t ES8388_DACCONTROL20 = 0x2a;
-const uint8_t ES8388_DACCONTROL21 = 0x2b;
-const uint8_t ES8388_DACCONTROL22 = 0x2c;
-const uint8_t ES8388_DACCONTROL23 = 0x2d;
-const uint8_t ES8388_DACCONTROL24 = 0x2e;
-const uint8_t ES8388_DACCONTROL25 = 0x2f;
-const uint8_t ES8388_DACCONTROL26 = 0x30;
-const uint8_t ES8388_DACCONTROL27 = 0x31;
-const uint8_t ES8388_DACCONTROL28 = 0x32;
-const uint8_t ES8388_DACCONTROL29 = 0x33;
-const uint8_t ES8388_DACCONTROL30 = 0x34;
+static const uint8_t ES8388_DACCONTROL1 = 0x17;
+static const uint8_t ES8388_DACCONTROL2 = 0x18;
+static const uint8_t ES8388_DACCONTROL3 = 0x19;
+static const uint8_t ES8388_DACCONTROL4 = 0x1a;
+static const uint8_t ES8388_DACCONTROL5 = 0x1b;
+static const uint8_t ES8388_DACCONTROL6 = 0x1c;
+static const uint8_t ES8388_DACCONTROL7 = 0x1d;
+static const uint8_t ES8388_DACCONTROL8 = 0x1e;
+static const uint8_t ES8388_DACCONTROL9 = 0x1f;
+static const uint8_t ES8388_DACCONTROL10 = 0x20;
+static const uint8_t ES8388_DACCONTROL11 = 0x21;
+static const uint8_t ES8388_DACCONTROL12 = 0x22;
+static const uint8_t ES8388_DACCONTROL13 = 0x23;
+static const uint8_t ES8388_DACCONTROL14 = 0x24;
+static const uint8_t ES8388_DACCONTROL15 = 0x25;
+static const uint8_t ES8388_DACCONTROL16 = 0x26;
+static const uint8_t ES8388_DACCONTROL17 = 0x27;
+static const uint8_t ES8388_DACCONTROL18 = 0x28;
+static const uint8_t ES8388_DACCONTROL19 = 0x29;
+static const uint8_t ES8388_DACCONTROL20 = 0x2a;
+static const uint8_t ES8388_DACCONTROL21 = 0x2b;
+static const uint8_t ES8388_DACCONTROL22 = 0x2c;
+static const uint8_t ES8388_DACCONTROL23 = 0x2d;
+static const uint8_t ES8388_DACCONTROL24 = 0x2e;
+static const uint8_t ES8388_DACCONTROL25 = 0x2f;
+static const uint8_t ES8388_DACCONTROL26 = 0x30;
+static const uint8_t ES8388_DACCONTROL27 = 0x31;
+static const uint8_t ES8388_DACCONTROL28 = 0x32;
+static const uint8_t ES8388_DACCONTROL29 = 0x33;
+static const uint8_t ES8388_DACCONTROL30 = 0x34;
 
 /**
  * @brief (un)mute one of the two outputs or main dac output of the ES8388 by switching of the output register bits.
@@ -72,7 +72,7 @@ const uint8_t ES8388_DACCONTROL30 = 0x34;
  * @param out
  * @param muted
  */
-void ES8388Component::mute(const OutputType out, const bool muted) {
+void ES8388Component::mute(OutputType out, bool muted) {
   uint8_t reg_addr;
   uint8_t mask_mute;
   uint8_t mask_val;
@@ -110,7 +110,7 @@ void ES8388Component::mute(const OutputType out, const bool muted) {
  * @param out which gain setting to control
  * @param vol 0-100 (100 is max)
  */
-void ES8388Component::volume(const OutputType out, const uint8_t vol) {
+void ES8388Component::volume(OutputType out, uint8_t vol) {
   const uint32_t max_vol = 100;  // max input volume value
 
   const int32_t max_vol_val =
