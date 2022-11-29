7e93d170a3d7ee1b9b5694c6121e4521d3f2f7ad
P4uLT <7680607+P4uLT@users.noreply.github.com>
2022-11-29 13:17:51 +0000

    feat(es8388) : init es8388 component

---
+++ components/es8388/es8388_component.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "esphome/components/i2c/i2c.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace es8388 {
+
+enum OutputType {
+  OUTPUT_TYPE_MAIN,  // this is the DAC output volume (both outputs)
+  OUTPUT_TYPE_OUT1,  // this is the additional gain for OUT1
+  OUTPUT_TYPE_OUT2   // this is the additional gain for OUT2
+};
+
+class ES8388Component : public Component, public i2c::I2CDevice {
+ public:
+  void setup() override;
+  void mute(OutputType out, bool muted);
+  void volume(OutputType out, uint8_t vol);
+  float get_setup_priority() const override { return setup_priority::LATE - 1; }
+};
+
+}  // namespace es8388
+}  // namespace esphome
